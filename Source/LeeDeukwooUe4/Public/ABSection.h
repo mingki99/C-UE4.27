d3d190dbbd58a77b05805904bdcbf9a5f071ae03
mingki99 <90670024+mingki99@users.noreply.github.com>
2022-06-29 21:40:50 +0900

    ABSection enum ESectionState COMPLETE, READY, BATTLE -> Gate Open(Rotation( 0.0f, -90.0f, 0.0f))
    Step4 Start = Gate Open

--- Source/LeeDeukwooUe4/Public/ABSection.h
+++ Source/LeeDeukwooUe4/Public/ABSection.h
@@ -2,8 +2,7 @@
 
 #pragma once
 
-// #include "LeeDeukwooUe4.h"
-#include "CoreMinimal.h"
+#include "LeeDeukwooUe4.h"
 #include "GameFramework/Actor.h"
 #include "ABSection.generated.h"
 
@@ -11,21 +10,52 @@ UCLASS()
 class LEEDEUKWOOUE4_API AABSection : public AActor
 {
 	GENERATED_BODY()
-	
-public:	
+
+public:
 	// Sets default values for this actor's properties
 	AABSection();
+	virtual void OnConstruction(const FTransform& Transform) override ;
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
+private:
+	enum class ESectionState : uint8
+	{
+		READY = 0,
+		BATTLE,
+		COMPLETE
+	};
+
+	void SetState(ESectionState NewState);
+	ESectionState CurrentState = ESectionState::READY;
+
+	void OperateGates(bool bOpen = true);
+
+
+
+public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 private:
-	/*UPROPERTY(VisibleAnywhere, Category = Mesh, Meta = (AllowPrivateAccess = true))
-	class UStaticMeshComponent* Mesh;*/
+	UPROPERTY(VisibleAnywhere, Category = Mesh, Meta = (AllowPrivateAccess = true))
+		TArray<UStaticMeshComponent*> GateMeshes;
+
+	UPROPERTY(VisibleAnywhere, Category = Trigger, Meta = (AllowPrivateAccess = true))
+	TArray<UBoxComponent*> GateTriggers;
+
+
+	UPROPERTY(VisibleAnywhere, Category = Mesh, Meta = (AllowPrivateAccess = true))
+		class UStaticMeshComponent* Mesh;
+
+	UPROPERTY(VisibleAnywhere, Category = Trigger, Meta = (AllowPrivateAccess = true))
+	class UBoxComponent* Trigger;
+
+	// 전투 없이 다음 섹션으로 통과 할 수 있는 bool형
+	UPROPERTY(EditAnywhere, Category = State, Meta = (AllowPrivateAccess = true))
+	bool bNoBattle;
+
 
 };
