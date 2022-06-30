48ed165e20fd1e9c3489e5b4f33d1ce28f64edab
mingki99 <90670024+mingki99@users.noreply.github.com>
2022-06-30 12:14:34 +0900

    ABSection enum ESectionState COMPLETE, READY, BATTLE -> Gate Open(Rotation( 0.0f, -90.0f, 0.0f))
    Step4 Start = Gate Open

--- Source/LeeDeukwooUe4/Private/ABSection.cpp
+++ Source/LeeDeukwooUe4/Private/ABSection.cpp
@@ -6,12 +6,12 @@
 // Sets default values
 AABSection::AABSection()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
-
 	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MESH"));
 	RootComponent = Mesh;
 
+	// Square 메쉬 변수에 저장.
 	FString AssetPath = TEXT("/Game/Book/StaticMesh/SM_SQUARE.SM_SQUARE");
 	static ConstructorHelpers::FObjectFinder<UStaticMesh> SM_SQUARE(*AssetPath);
 	if (SM_SQUARE.Succeeded())
@@ -23,6 +23,15 @@ AABSection::AABSection()
 		ABLOG(Error, TEXT("Failed to load staticmesh asset. : %s"), *AssetPath);
 	}
 
+	// BoxComponent 크기, 컴포넌트 관계 ,위치.
+	Trigger = CreateDefaultSubobject<UBoxComponent>(TEXT("TRIGGER"));
+	Trigger->SetBoxExtent(FVector(775.0f, 775.0f, 300.0f));
+	Trigger->SetupAttachment(RootComponent);
+	Trigger->SetRelativeLocation(FVector(0.0f, 0.0f, 250.0f));
+	// 만들어준 콜리전으로 ABCharacter콜리전만 겹치는 콜리전 지정
+	Trigger->SetCollisionProfileName(TEXT("ABTrigger"));
+
+	// Gate메쉬 변수에 저장.
 	FString GateMeshPath = TEXT("/Game/Book/StaticMesh/SM_GATE.SM_GATE");
 	static ConstructorHelpers::FObjectFinder<UStaticMesh> SM_GATE(*GateMeshPath);
 	if (!SM_GATE.Succeeded())
@@ -30,29 +39,108 @@ AABSection::AABSection()
 		ABLOG(Error, TEXT("Failed to load Staticmesh : %s"), *GateMeshPath);
 	}
 
-	static FName GateSockets[] = { {TEXT("+XGate")},{TEXT("-XGate")}, {TEXT("+YGate")} ,{TEXT("-YGate")}};
+	// Square에 있는 Socket의 이름 배열로 저장.
+	static FName GateSockets[] = { {TEXT("+XGate")},{TEXT("-XGate")}, {TEXT("+YGate")} ,{TEXT("-YGate")} };
+
+	// Square메쉬 4개의 Socket안에 들어가는 요소들
 	for (FName GateSocket : GateSockets)
 	{
 		ABCHECK(Mesh->DoesSocketExist(GateSocket));
+
+		// 바로 생성하여 다른 소켓에 메쉬를 지정해주고 위치
 		UStaticMeshComponent* NewGate = CreateDefaultSubobject<UStaticMeshComponent>(*GateSocket.ToString());
 		NewGate->SetStaticMesh(SM_GATE.Object);
 		NewGate->SetupAttachment(RootComponent, GateSocket);
 		NewGate->SetRelativeLocation(FVector(0.0f, -80.5f, 0.0f));
-		GateMesh.Add(NewGate);
+		GateMeshes.Add(NewGate);
 
-
-
+		// TriggerBox 크기, 컴포넌트 관계, 위치 지정
+		UBoxComponent* NewGateTrigger = CreateDefaultSubobject<UBoxComponent>(*GateSocket.ToString().Append(TEXT("Trigger")));
+		NewGateTrigger->SetBoxExtent(FVector(100.0f, 100.0f, 300.0f));
+		NewGateTrigger->SetupAttachment(RootComponent, GateSocket);
+		NewGateTrigger->SetRelativeLocation(FVector(70.0f, 0.0f, 250.0f));
+		// 만들어준 콜리전으로 ABCharacter콜리전만 겹치는 콜리전 지정
+		NewGateTrigger->SetCollisionProfileName(TEXT("ABTrigger"));
+		GateTriggers.Add(NewGateTrigger);
 	}
 
-	
+
+	bNoBattle = false;
+
+
+
 }
 
 // Called when the game starts or when spawned
 void AABSection::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	SetState(bNoBattle ? ESectionState::COMPLETE : ESectionState::READY);
 }
+
+void AABSection::SetState(ESectionState NewState)
+{
+	switch (NewState)
+	{
+	case ESectionState::READY:
+	{
+		Trigger->SetCollisionProfileName(TEXT("ABTrigger"));
+		for (UBoxComponent* GateTrigger : GateTriggers)
+		{
+			GateTrigger->SetCollisionProfileName(TEXT("NoCollsion"));
+		}
+
+		OperateGates(true);
+		break;
+	}
+
+	case ESectionState::BATTLE:
+	{
+		Trigger->SetCollisionProfileName(TEXT("NoCollsion"));
+		for (UBoxComponent* GateTrigger : GateTriggers)
+		{
+			GateTrigger->SetCollisionProfileName(TEXT("NoCollsion"));
+		}
+
+		OperateGates(false);
+		break;
+	}
+
+	case ESectionState::COMPLETE:
+	{
+		Trigger->SetCollisionProfileName(TEXT("NoCollsion"));
+		for (UBoxComponent* GateTrigger : GateTriggers)
+		{
+			GateTrigger->SetCollisionProfileName(TEXT("ABTrigger"));
+		}
+
+		OperateGates(true);
+		break;
+	}
+
+	}
+
+	CurrentState = NewState;
+}
+
+
+void AABSection::OperateGates(bool bOpen)
+{
+	for (UStaticMeshComponent* Gate : GateMeshes)
+	{
+		// Mesh를 회전하여 문이 열리도록 보이게한다.
+		Gate->SetRelativeRotation(bOpen ? FRotator(0.0f, -90.0f, 0.0f) : FRotator::ZeroRotator);
+	}
+}
+
+
+void AABSection::OnConstruction(const FTransform& Transform)
+{
+	Super::OnConstruction(Transform);
+	SetState(bNoBattle ? ESectionState::COMPLETE : ESectionState::READY);
+}
+
 
 // Called every frame
 void AABSection::Tick(float DeltaTime)
